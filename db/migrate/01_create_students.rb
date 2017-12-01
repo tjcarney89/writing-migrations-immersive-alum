8554bffba2bcdba5c936731ee0437d9bb0540e51
TJ Carney <tjcarney@stny.rr.com>
2017-12-01 17:12:34 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,19 @@
 class CreateStudents < ActiveRecord::Migration
+
+  def create_table(name)
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT
+      )
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
